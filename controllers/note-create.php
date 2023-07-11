6030751c3a0fcf73bb01facc73c571b56d528b02
Ahmad Musanejad <Ahmadm7599@gmail.com>
2023-07-11 14:45:47 +0330

    learn form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,25 +1,22 @@
 <?php
+require('Validator.php');
 $config = require("config.php");
-$db = new Database($config['database']);
-
 $heading = "create new note";
 
-/* first way */
-// if(isset($_POST['body']))
-// {
-//     $db->query("insert into notes (`body` , `user_id`) VALUES (:body , :user_id)" , [
-//         'body' => $_POST['body'],
-//         'user_id' => 1
-//     ]);
-// }
-// second way
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
+    $db = new Database($config['database']);
+    $errors = [];
+    if(Validator::string($_POST['body']))
+    {
+        $errors['body'] = 'body is incorrect!';
+    }
+    if(empty($errors)){
     $db->query("insert into notes (`body` , `user_id`) VALUES (:body , :user_id)" , [
         'body' => $_POST['body'],
         'user_id' => 1
     ]);
 }
+}
 
-   
 require("views/note-create.view.php");
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,8 +4,11 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <div class="px-4 py-6 sm:px-0">
-        <form method="POST" class="flex items-center gap-3">
-            <textarea rows="4" name="body"></textarea>
+        <form method="POST">
+           <div><textarea rows="4" name="body"><?= isset($_POST['body']) ? $_POST['body'] : ''  ?></textarea></div>
+            <?php if(!empty($errors)): ?>
+            <div class="text-red-400 text-xs m-2"><?= $errors['body'] ?></div>
+            <?php endif; ?>
             <button class="bg-blue-400 px-4 py-2 text-white rounded hover:bg-red-400" type="submit">create</button>
         </form>
       </div>
